71fc200313d380a7354db0f767b1af1aba30a1de
Ahmed Sami <ahmedsami03@protonmail.com>
2024-04-27 01:29:46 +0200

    Add tests

--- src/test/kotlin/com/github/tautastic/hamcrest2assertj/plugin/HamcrestAssertInspectionTest.kt
+++ src/test/kotlin/com/github/tautastic/hamcrest2assertj/plugin/HamcrestAssertInspectionTest.kt
@@ -1,12 +1,12 @@
 package com.github.tautastic.hamcrest2assertj.plugin
 
+import com.github.tautastic.hamcrest2assertj.InspectionBundle
 import com.intellij.codeInsight.daemon.impl.HighlightInfo
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import junit.framework.TestCase
-import org.jetbrains.annotations.NotNull
 
-@TestDataPath("\$CONTENT_ROOT/src/test/testData")
+
+@TestDataPath("\$CONTENT_ROOT/src/test/resources")
 class HamcrestAssertInspectionTest : LightJavaCodeInsightFixtureTestCase() {
     override fun setUp() {
         super.setUp()
@@ -14,14 +14,35 @@ class HamcrestAssertInspectionTest : LightJavaCodeInsightFixtureTestCase() {
     }
 
     fun testQualifiedAssertThat() {
-        doTest("QualifiedAssertThat")
+        doTest()
     }
 
-    private fun doTest(@NotNull testName: String) {
-        myFixture.configureByFile("$testName.java")
-        val highlightInfos: List<HighlightInfo> = myFixture.doHighlighting()
-        TestCase.assertFalse(highlightInfos.isEmpty())
+    private fun doTest(toSelect: Int) {
+        myFixture.configureByFile(getBeforeFile())
+        myFixture.enableInspections(HamcrestAssertInspection::class.java)
+        val highlightInfo =
+            myFixture.doHighlighting()
+                .filter(this::filterHighlightInfos)[toSelect]
+        assertNotNull("cannot find highlight", highlightInfo)
     }
 
-    override fun getTestDataPath() = "src/test/testData/assertThat"
+    private fun filterHighlightInfos(it: HighlightInfo?): Boolean {
+        return it?.inspectionToolId == "Hamcrest2AssertJ" && it.description == InspectionBundle.message(
+            "inspection.hamcrest.assert.that.problem.descriptor"
+        )
+    }
+
+    private fun doTest() {
+        doTest(0)
+    }
+
+    private fun getResultFile(): String {
+        return getTestName(true) + ".after.java"
+    }
+
+    private fun getBeforeFile(): String {
+        return getTestName(true) + ".java"
+    }
+
+    override fun getTestDataPath() = "src/test/resources/assertThat"
 }
--- src/test/resources/assertThat/QualifiedAssertThat.java
+++ src/test/resources/assertThat/QualifiedAssertThat.java
@@ -1,28 +1,3 @@
-package org.example;
-
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.Test;
-
-import java.time.LocalDateTime;
-
-public class CommonUtilsTest {
-
-    private static class CommonUtils {
-        public static boolean isLocalDateTimeBetween(final LocalDateTime toCheck, final LocalDateTime from, final LocalDateTime to) {
-            if (Objects.isNull(toCheck) || Objects.isNull(from) || Objects.isNull(to)) {
-                return false;
-            }
-
-            return toCheck.isAfter(from) && toCheck.isBefore(to);
-        }
-    }
-
-    @Test
-    public void testIsLocalDateTimeBetween() {
-        final boolean result = CommonUtils.isLocalDateTimeBetween(null, null, null);
-
-        MatcherAssert.assertThat("wrong result", result, Matchers.is(false));
-    }
-
+public void testIsFalse() {
+    MatcherAssert.assertThat("wrong result", result, Matchers.is(false));
 }
